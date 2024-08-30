cff8603393ddf9e13fbd462c5bc19d18cf14d350
dragan002 <draganvujic29@gmail.com>
2024-08-30 17:50:01 +0200

    "Added plugin security check, created SimpleContactForm class, and registered custom post type 'simple_contact_form'"

--- simple-contact-form/simple-contact-form.php
+++ simple-contact-form/simple-contact-form.php
@@ -7,3 +7,39 @@
  * Version: 1.0.0
  * Text Domain: simple-contact-form
  */
+
+if(!defined('ABSPATH')) {
+    echo "What are you trying to do?";
+    exit;
+ }
+ //If ABSPATH is not defined,
+ // that the plugin is being accessed directly 
+
+ class SimpleContactForm  {
+
+    public function __construct()
+    {
+        add_action('init', array($this, 'CreateCustomPostType'));
+    }
+
+    public function CreateCustomPostType()
+    {
+        $args = array(
+            'public'                =>true,
+            'has_archive'           => true,
+            'supports'              =>array('title'),
+            'exclude_from_search'   => true,
+            'publicly_queryable'    => false,
+            'capability'            => 'manage_options',
+            'labels' => array(
+                'name'          => 'Contact Form',
+                'singular_name' => 'Contact Form Entry'
+            ),
+            'menu_icon'         => 'dashicons-media-text'
+        );
+
+        register_post_type('simple_contact_form', $args);
+    }
+ }
+
+ new SimpleContactForm;
