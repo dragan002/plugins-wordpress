781c84fb9f09948afe3786e9519a147198894428
dragan002 <draganvujic29@gmail.com>
2024-09-07 12:57:32 +0200

    Creating fields

--- contact-plugin/includes/options-page.php
+++ contact-plugin/includes/options-page.php
@@ -10,7 +10,7 @@ add_action('carbon_fields_register_fields', 'create_options_page');
 
 function load_carbon_fields()
 {
-    if(class_exists('\Carbon_Fields\Carbon_Fields')) {
+    if (class_exists('\Carbon_Fields\Carbon_Fields')) {
         error_log('Carbon Fields is loaded');
         \Carbon_Fields\Carbon_Fields::boot();
     } else {
@@ -20,9 +20,20 @@ function load_carbon_fields()
 
 function create_options_page()
 {
-Container::make( 'theme_options', __( 'Theme Options' ) )
-    ->add_fields( array(
-        Field::make( 'text', 'crb_facebook_url', __( 'Facebook URL' ) ),
-        Field::make( 'textarea', 'crb_footer_text', __( 'Footer Text' ) )
-    ) );
+    Container::make('theme_options', __('Contact Form'))
+        ->set_icon('dashicons-media-text')
+        ->add_fields(array(
+
+            Field::make('checkbox', 'contact_plugin_active', __('Active')),
+
+            Field::make('text', 'contact_plugin_recipients', __('Recipients email'))
+                ->set_attribute('placeholder', 'eg. youremail@gmail.com')
+                ->set_help_text('The email that the form is submitted to')
+                ->set_required(true)
+                ->set_attribute('type', 'email'),
+
+            Field::make('textarea', 'contact_plugin_message', __('confirmation message'))
+                ->set_attribute('placeholder', 'Enter Confirmation Message')
+                ->set_help_text('Type the message you want the submitter to recieve')
+        ));
 }
